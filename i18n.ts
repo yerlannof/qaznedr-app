78dab5d59329e40e603c5529878a12cc33dd354b
Yerlan Kulumgariyev <yerlan@qaznedr.kz>
2025-09-14 14:19:09 +0500

    Add i18n.ts config file in root directory for next-intl

---
+++ i18n.ts
@@ -0,0 +1,19 @@
+import { getRequestConfig } from 'next-intl/server';
+import { locales } from './config';
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
+
+  // Validate that the incoming locale is valid
+  if (!locale || !locales.includes(locale as any)) {
+    return {
+      locale: 'ru',
+      messages: (await import(`../../messages/ru.json`)).default,
+    };
+  }
+
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default,
+  };
+});

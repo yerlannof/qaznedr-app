45a20a251dfe9d99b82e93d95e9880d6d35ee83c
Yerlan Kulumgariyev <yerlan@qaznedr.kz>
2025-09-14 15:54:31 +0500

    Add debug page outside i18n system

---
+++ src/app/debug/page.tsx
@@ -0,0 +1,10 @@
+export default function DebugPage() {
+  return (
+    <html>
+      <body>
+        <h1>Debug Page - No i18n</h1>
+        <p>If this works, the issue is with i18n configuration</p>
+      </body>
+    </html>
+  );
+}
